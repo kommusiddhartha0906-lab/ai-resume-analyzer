fe4b2f0260e4b06e3a8ae49a048eba047624692a
Siddhartha <kommusiddhartha0906@gmail.com>
2026-04-17 13:05:39 +0530

    added gitignore and backend setup

--- server/server.js
+++ server/server.js
@@ -2,39 +2,22 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+console.log("EXPRESS SERVER STARTED");
+
 dotenv.config();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
 // test route
 app.get("/", (req, res) => {
-  res.send("AI Resume Analyzer Backend Running");
+  res.send("EXPRESS WORKING");
 });
 
-// basic API route
-app.post("/analyze", (req, res) => {
-  const { resume } = req.body;
+const PORT = 5050;
 
-  if (!resume) {
-    return res.status(400).json({ error: "Resume is required" });
-  }
-
-  // dummy logic (we'll replace with AI later)
-  const wordCount = resume.split(" ").length;
-
-  res.json({
-    message: "Resume analyzed successfully",
-    wordCount: wordCount,
-    score: Math.min(100, wordCount), // fake score for now
-  });
-});
-
-// server start
-const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
